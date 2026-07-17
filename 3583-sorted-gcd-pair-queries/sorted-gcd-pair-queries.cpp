d870a30c683d8a495373def476cc90fcc4d18dea
AkSharma2003 <ankit824012@gmail.com>
2026-07-17 12:43:38 +0530

    Time: 37 ms (82.67%) | Memory: 129.8 MB (66.67%) - LeetSync

---
+++ 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+        int mx = *max_element(nums.begin(), nums.end());
+        vector<int> freq(mx + 1, 0);
+        for (int x : nums)
+            freq[x]++;
+
+        // cnt[g] = number of pairs whose gcd is exactly g
+        vector<long long> cnt(mx + 1, 0);
+
+        // Count pairs divisible by g
+        for (int g = 1; g <= mx; g++) {
+            long long c = 0;
+
+            for (int j = g; j <= mx; j += g)
+                c += freq[j];
+
+            cnt[g] = c * (c - 1) / 2;
+        }
+
+        // Inclusion-Exclusion
+        for (int g = mx; g >= 1; g--) {
+            for (int j = 2 * g; j <= mx; j += g)
+                cnt[g] -= cnt[j];
+        }
+
+        // Prefix count
+        vector<long long> prefix(mx + 1, 0);
+
+        for (int i = 1; i <= mx; i++)
+            prefix[i] = prefix[i - 1] + cnt[i];
+
+        vector<int> ans;
+
+        for (long long q : queries) {
+
+            int g = lower_bound(prefix.begin() + 1,
+                                prefix.end(),
+                                q + 1) - prefix.begin();
+
+            ans.push_back(g);
+        }
+
+        return ans;
+    }
+};
