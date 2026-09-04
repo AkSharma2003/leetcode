3b6304196482b045e30f4d9c39e2f773ea6ce163
AkSharma2003 <ankit824012@gmail.com>
2026-09-04 09:20:24 +0530

    Time: 3 ms (25.17%) | Memory: 31.1 MB (66.39%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -5,13 +5,26 @@ public:
         if(n<2){
             return k<=0?k:0;
         }
+        vector<int> mx;
+
+        int num=nums[0];
         for(int i=0;i<n;i++){
-            int max=*max_element(nums.begin(),nums.begin()+i);
-            int min=*min_element(nums.begin()+i,nums.end());
-            if(max-min<=k){
-                return i;
-            }
+            num=nums[i]>num?nums[i]:num;
+            mx.push_back(num);
         }
+
+        vector<int>mn;
+        num=nums[n-1];
+        for(int i=n-1;i>=0;i--){
+            num=nums[i]<num?nums[i]:num;
+            mn.push_back(num);
+        }
+
+        reverse(mn.begin(),mn.end());
+        for(int i=0;i<n;i++){
+            if(mx[i]-mn[i]<=k) return i;
+        }
+        
         return -1;
     }
 };
