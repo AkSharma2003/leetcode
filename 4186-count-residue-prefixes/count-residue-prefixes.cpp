dbbbd0a24e7a53f349795e71b4f5fc0519ff0dbd
AkSharma2003 <ankit824012@gmail.com>
2026-09-16 15:40:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (51.37%) - LeetSync

--- 4186-count-residue-prefixes/count-residue-prefixes.cpp
+++ 4186-count-residue-prefixes/count-residue-prefixes.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int residuePrefixes(string s) {
-        int ct=0,i=0;
-        set<char> st;
-
-        for(char c:s){
-            i++;
-            st.insert(c);
-            if(i%3==st.size()) ct++;
+        unordered_set<char> st;
+        int result = 0;
+        for(int i = 0; i < s.length(); ++i){
+            st.insert(s[i]);
+            if (st.size() == (i + 1) % 3)
+                result++;
+            if (st.size() > 2)
+                break;
         }
-
-        return ct;
+        return result;
     }
 };
